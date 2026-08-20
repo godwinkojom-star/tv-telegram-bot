25c5c291bb9a261a5674beff5b1ce906f5e6bf4d
godwinkojom-star <godwinkojom@gmail.com>
2026-08-20 20:58:51 +0000

    app.py

--- app.py
+++ app.py
@@ -32,6 +32,9 @@ from flask import Flask, jsonify
 import requests
 from PIL import Image, ImageDraw, ImageFont
 import psycopg
+from dotenv import load_dotenv
+
+load_dotenv()
 
 try:
     from pywebpush import webpush, WebPushException
